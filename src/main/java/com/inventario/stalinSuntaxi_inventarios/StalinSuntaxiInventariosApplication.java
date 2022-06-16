020001eb89dbd2bb13d3c36dfa737c41a1acafba
Stalin Suntaxi <stalin.suntaxi@bayteq.com>
2022-06-16 17:29:58 -0500

    prueba

---
+++ src/main/java/com/inventario/stalinSuntaxi_inventarios/StalinSuntaxiInventariosApplication.java
@@ -0,0 +1,13 @@
+package com.inventario.stalinSuntaxi_inventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StalinSuntaxiInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StalinSuntaxiInventariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventario/stalinSuntaxi_inventarios/StalinSuntaxiInventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventario.stalinSuntaxi_inventarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StalinSuntaxiInventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
